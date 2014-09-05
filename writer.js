c08f54487f1482740e3cd7864f99805b818482de
Trevor Basinger <trevor.basinger@gmail.com>
2014-09-05 08:44:55 -0500

    Made chain operation lazy. Also attempted my first transformer.

--- writer.js
+++ writer.js
@@ -2,9 +2,14 @@ var Tuple2 = require('fantasy-tuples').Tuple2;
 
 var id = function (x) { return x; };
 
+var overrideMethod = function ( obj, method, func ) {
+  obj[method] = func;
+  return obj;
+}
+
 var Writer = function(x, y) {
   this.run = function() {
-    return Tuple2(x, y);
+    return Tuple2(x, y || { concat: id });
   };
   return this;
 };
@@ -14,27 +19,73 @@ Writer.of = function(x) {
 };
 
 Writer.prototype.chain = function(f) {
-  var result = this.run();
-  var t = f(result._1).run();
-  return new Writer(t._1, result._2.concat(t._2));
+  var self = this;
+  return overrideMethod(new Writer(), 'run', function() {
+    var result = self.run();
+    var t = f(result._1).run();
+    return Tuple2(t._1, result._2.concat(t._2));
+  });
 };
 
 Writer.prototype.tell = function(y) {
-  var result = this.run();
-  return new Writer(null, result._2.concat(y));
+  var self = this;
+  return overrideMethod(new Writer(), 'run', function () {
+    var result = self.run();
+    return new Tuple2(null, result._2.concat(y));
+  });
 }
 
 Writer.prototype.map = function(f) {
   return this.chain(function(a) {
-    return new Writer(f(a), []);
+    return new Writer(f(a), { concat: id });
   });
 };
 
-Writer.prototype.ap = function(a) {
-  return this.chain(function(f) {
-    return a.map(f);
+Writer.prototype.ap = function(b) {
+  return this.chain(function(a) {
+    return b.map(a);
   });
 };
+
+Writer.WriterT = function(M) {
+
+  var WriterT = function(x, y) {
+    this.run = function() {
+      return Tuple2(x, y || { concat: id });
+    };
+  };
+
+  WriterT.of = function(x) {
+    return new WriterT(M.of(x), { concat: id });
+  };
+
+  WriterT.lift = function(m) {
+    return new WriterT(m, { concat: id } );
+  };
+
+  WriterT.prototype.chain = function(f) {
+    var self = this;
+    return overrideMethod(new WriterT(), 'run', function() {
+      var result = self.run();
+      var m = result._1.chain(f);
+      return Tuple2(m, result._2);
+    });
+  };
+
+  WriterT.prototype.map = function(f) {
+    return this.chain(function(a) {
+      return WriterT.of(a._1.map(f), { concat: id });
+    });
+  };
+
+  WriterT.prototype.ap = function(b) {
+    return this.chain(function(f) {
+      return b.map(f);
+    });
+  };
+
+  return WriterT;
+}
 
 if(typeof module != 'undefined')
   module.exports = Writer;
