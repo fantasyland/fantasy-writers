da0e141e788f23e0ac5390a42e0c52d7b6cf3277
SimonRichardson <stickupkid@gmail.com>
2016-02-07 11:51:02 +0000

    Fixes the issue around creation of the writer

--- src/writer.js
+++ src/writer.js
@@ -1,39 +1,47 @@
+'use strict';
+
 const daggy = require('daggy');
 
-const {identity} = require('fantasy-combinators');
-const {Tuple2} = require('fantasy-tuples');
+const { of, chain, map, ap } = require('fantasy-land');
+const { identity } = require('fantasy-combinators');
+const { Tuple2 } = require('fantasy-tuples');
 
-const Writer = daggy.tagged('run');
+const Writer = M => {
 
-Writer.of = function(x) {
-  return Writer(() => Tuple2(x, {concat: identity}));
+    const Writer = daggy.tagged('run');
+
+    Writer.of = function(x) {
+        return Writer(() => Tuple2(x, M.empty()));
+    };
+
+    Writer.prototype.chain = function(f) {
+        return Writer(() => {
+            const result = this.run();
+            const t = f(result._1).run();
+            return Tuple2(t._1, result._2.concat(t._2));
+        });
+    };
+
+    Writer.prototype.tell = function(y) {
+        return Writer(() => {
+            const result = this.run();
+            return Tuple2(null, result._2.concat(y));
+        });
+    };
+
+    Writer.prototype.map = function(f) {
+        return Writer(() => {
+            const result = this.run();
+            return Tuple2(f(result._1), result._2);
+        });
+    };
+
+    Writer.prototype.ap = function(b) {
+        return this.chain((a) => b.map(a));
+    };
+
+    return Writer;
+
 };
 
-Writer.prototype.chain = function(f) {
-  return Writer(() => {
-    const result = this.run();
-    const t = f(result._1).run();
-    return Tuple2(t._1, result._2.concat(t._2));
-  });
-};
-
-Writer.prototype.tell = function(y) {
-  return Writer(() => {
-    const result = this.run();
-    return Tuple2(null, result._2.concat(y));
-  });
-};
-
-Writer.prototype.map = function(f) {
-  return Writer(() => {
-    const result = this.run();
-    return Tuple2(f(result._1), result._2);
-  });
-};
-
-Writer.prototype.ap = function(b) {
-  return this.chain((a) => b.map(a));
-};
-
-if (typeof module != 'undefined')
-  module.exports = Writer;
+module.exports = Writer;
--- test/writer.js
+++ test/writer.js
@@ -3,8 +3,10 @@ const applicative = require('fantasy-check/src/laws/applicative');
 const functor = require('fantasy-check/src/laws/functor');
 const monad = require('fantasy-check/src/laws/monad');
 
-const {constant, identity} = require('fantasy-combinators');
-const {Tuple2} = require('fantasy-tuples');
+const { tagged } = require('daggy');
+
+const { constant, identity } = require('fantasy-combinators');
+const { Tuple2 } = require('fantasy-tuples');
 
 const Identity = require('fantasy-identities');
 const Writer = require('../fantasy-writers');
@@ -13,38 +15,52 @@ function run(a) {
     return Identity.of(a.run()._1);
 }
 
+const Seq = tagged('x');
+
+Seq.empty = () => Seq([]);
+Seq.prototype.empty = Seq.empty;
+
+Seq.prototype.concat = function(x) {
+    return Seq(this.x.concat(x.x));
+};
+Seq.prototype.length = function() {
+    return this.x.length;
+};
+
+const Writerʹ = Writer(Seq);
+
 exports.writer = {
 
     // Applicative Functor tests
-    'All (Applicative)': applicative.laws(λ)(Writer, run),
-    'Identity (Applicative)': applicative.identity(λ)(Writer, run),
-    'Composition (Applicative)': applicative.composition(λ)(Writer, run),
-    'Homomorphism (Applicative)': applicative.homomorphism(λ)(Writer, run),
-    'Interchange (Applicative)': applicative.interchange(λ)(Writer, run),
+    'All (Applicative)': applicative.laws(λ)(Writerʹ, run),
+    'Identity (Applicative)': applicative.identity(λ)(Writerʹ, run),
+    'Composition (Applicative)': applicative.composition(λ)(Writerʹ, run),
+    'Homomorphism (Applicative)': applicative.homomorphism(λ)(Writerʹ, run),
+    'Interchange (Applicative)': applicative.interchange(λ)(Writerʹ, run),
 
     // Functor tests
-    'All (Functor)': functor.laws(λ)(Writer.of, run),
-    'Identity (Functor)': functor.identity(λ)(Writer.of, run),
-    'Composition (Functor)': functor.composition(λ)(Writer.of, run),
+    'All (Functor)': functor.laws(λ)(Writerʹ.of, run),
+    'Identity (Functor)': functor.identity(λ)(Writerʹ.of, run),
+    'Composition (Functor)': functor.composition(λ)(Writerʹ.of, run),
 
     // Monad tests
-    'All (Monad)': monad.laws(λ)(Writer, run),
-    'Left Identity (Monad)': monad.leftIdentity(λ)(Writer, run),
-    'Right Identity (Monad)': monad.rightIdentity(λ)(Writer, run),
-    'Associativity (Monad)': monad.associativity(λ)(Writer, run),
+    'All (Monad)': monad.laws(λ)(Writerʹ, run),
+    'Left Identity (Monad)': monad.leftIdentity(λ)(Writerʹ, run),
+    'Right Identity (Monad)': monad.rightIdentity(λ)(Writerʹ, run),
+    'Associativity (Monad)': monad.associativity(λ)(Writerʹ, run),
 
     // Manual tests
     'when testing the of': λ.check(
-        (a) => Writer.of(a).run()._1 === a,
+        (a) => Writerʹ.of(a).run()._1 === a,
         [λ.AnyVal]
     ),
     'when testing the of for the second value': λ.check(
-        (a) => Writer.of(a).run()._2.concat([]).length === 0,
+        (a) => Writerʹ.of(a).run()._2.concat(Seq([])).length() === 0,
         [λ.AnyVal]
     ),
     'when testing the chain, returns correct value': λ.check(
         (a, b) => {
-            return Writer.of(a).chain((x) => Writer.of(x + b)).run()._1 === a + b;
+            return Writerʹ.of(a).chain((x) => Writerʹ.of(x + b)).run()._1 === a + b;
         },
         [λ.AnyVal, λ.AnyVal]
     )
